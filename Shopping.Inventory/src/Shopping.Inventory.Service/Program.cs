83ce072de5b6088d1fa62faedda3ac534ea57eeb
rolysr <rolsanchez@yandex.com>
2022-08-25 16:49:39 -0400

    Timeouts and exponential backoff implemented

--- Shopping.Inventory/src/Shopping.Inventory.Service/Program.cs
+++ Shopping.Inventory/src/Shopping.Inventory.Service/Program.cs
@@ -1,3 +1,5 @@
+using Polly;
+using Polly.Timeout;
 using Shopping.Common.MongoDB;
 using Shopping.Inventory.Service.Models;
 using Shopping.Inventory.Service.Clients;
@@ -8,10 +10,22 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddMongo()
                 .AddMongoRepository<InventoryItem>("inventoryitems");
 
+Random jitterer = new Random();
+
 builder.Services.AddHttpClient<CatalogClient>(client => 
 {
     client.BaseAddress = new Uri("https://localhost:7085");
-});
+})
+.AddTransientHttpErrorPolicy(builder => builder.WaitAndRetryAsync(
+    5,
+    retryAttemp => TimeSpan.FromSeconds(Math.Pow(2, retryAttemp)) 
+                    + TimeSpan.FromMilliseconds(jitterer.Next(0,1000))
+))
+.AddTransientHttpErrorPolicy(builder => builder.Or<TimeoutRejectedException>().CircuitBreakerAsync(
+    3,
+    TimeSpan.FromSeconds(15)
+))
+.AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(1));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
